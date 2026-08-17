3d4058bb19dbfebed94421835c550aaa16230c38
Mikhael-agung <mikhaelagung3@gmail.com>
2026-08-17 05:00:16 +0000

    feat(file-processor): enhance file processing and update lesson state

--- build-a-file-processor/server.js
+++ build-a-file-processor/server.js
@@ -1,3 +1,27 @@
-
 const fs = require('fs');
-console.log(fs);
+const fsPromises = require('fs/promises');
+
+async function main(){
+    const data = await fsPromises.readFile("assets/poem.txt", { encoding: 'utf-8' });
+    console.log(data);
+}
+
+fs.writeFileSync('assets/output.txt', 'Hello, freeCodeCamp!');
+
+fs.appendFileSync('assets/output.txt', '\nSecond Line')
+
+const exists = fs.existsSync('assets/output.txt');
+console.log(exists);
+
+const entries = fs.readdirSync('assets');
+console.log(entries);
+
+const buf = Buffer.alloc('8, 0xff');
+console.log(buf)
+
+console.log(buf.toString('hex'));
+console.log(buf.toString('base64'))
+
+
+
+main();
