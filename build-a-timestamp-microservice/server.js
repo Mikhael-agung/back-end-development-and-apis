a11d2801f478f9518bafcda6fe9a1ae7beb04a63
Mikhael-agung <mikhaelagung3@gmail.com>
2026-08-18 13:51:47 +0000

    feat: update project settings and implement chat server with WebSocket support

--- build-a-timestamp-microservice/server.js
+++ build-a-timestamp-microservice/server.js
@@ -44,6 +44,6 @@ app.get('/api/:date?', (req, res) => {
   });
 });
 
-const listener = app.listen(process.env.PORT || 3000, () => {
+const listener = app.listen(process.env.PORT || 8000, () => {
   console.log('Your app is listening on port ' + listener.address().port);
 });
